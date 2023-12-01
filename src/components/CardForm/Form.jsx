063418da6d139fdfeff1dc4b01a0ca3db836ccc0
Alexandr Borodinov <yaborodinov@gmail.com>
2023-12-01 09:26:22 +0200

    completed validations

--- src/components/CardForm/Form.jsx
+++ src/components/CardForm/Form.jsx
@@ -7,15 +7,137 @@ import CVVIcon from '../../assets/img/icons/singleColorIcons/icons8_lock.svg'
 import HolderIcon from '../../assets/img/icons/singleColorIcons/ant-design_user-outlined.svg'
 import Button from '../Button'
 
+const required = 'required'
+
 const Form = () => {
   const [emailValue, setEmailValue] = useState('')
   const [cardNumber, setCardNumber] = useState('')
   const [expiry, setExpiry] = useState('')
   const [cvv, setCvv] = useState('')
   const [holderName, setHolderName] = useState('')
+  const [emailError, setEmailError] = useState('')
+  const [cardError, setCardError] = useState(false)
+  const [expiryError, setExpiryError] = useState(false)
+  const [cvvError, setCvvError] = useState(false)
+  const [holderError, setHolderError] = useState(false)
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    !emailValue && setEmailError(required)
+    !cardNumber && setCardError(required)
+    !expiry && setExpiryError(required)
+    !cvv && setCvvError(required)
+    !holderName && setHolderError(required)
+
+    if (
+      !emailValue ||
+      !cardNumber ||
+      !expiry ||
+      !cvv ||
+      !holderName ||
+      emailError ||
+      cardError ||
+      expiryError ||
+      cvvError ||
+      holderError
+    ) {
+      return
+    }
+
+    const cardNumberWithoutSpaces = parseInt(cardNumber.replace(/\s/g, ''), 10)
+
+    const formData = {
+      email: emailValue,
+      cardNumber: cardNumberWithoutSpaces,
+      expiry: expiry,
+      cvv: cvv,
+      holderName: holderName,
+    }
+    // here we can send data to BE
+    console.log('Form Data:', formData)
+  }
+
+  const isValidEmail = email => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    const result = emailRegex.test(email)
+    if (!result && email) {
+      setEmailError('Invalid email address')
+    } else if (!result && !email) {
+      setEmailError(required)
+    }
+  }
+
+  const isValidCardNumber = value => {
+    const cleanedValue = value.replace(/\D/g, '')
+    const result = /^\d{16}$/.test(cleanedValue) ? true : false
+    if (!result && value) {
+      setCardError('Your card number is invalid')
+    } else if (!result && !value) {
+      setCardError(required)
+    }
+  }
+
+  const isValidExpiryDate = input => {
+    const regex = /^(0[1-9]|1[0-2])\/(20\d{2})$/
+    if (!regex.test(input) && input) {
+      setExpiryError("Your card's expiration date is invalid")
+      return
+    } else if (!regex.test(input) && !input) {
+      setExpiryError(required)
+      return
+    }
+    const [, month, year] = input.match(regex)
+    const currentYear = new Date().getFullYear()
+    const validYear = currentYear + 5
+
+    return (
+      parseInt(year, 10) >= currentYear &&
+      parseInt(year, 10) <= validYear &&
+      parseInt(month, 10) >= 1 &&
+      parseInt(month, 10) <= 12
+    )
+  }
+
+  const isValidCVV = input => {
+    const regex = /^\d{3}$/
+    if (!regex.test(input) && input) {
+      setCvvError('Invalid value')
+    } else if (!regex.test(input) && !input) {
+      setCvvError(required)
+    }
+  }
+
+  const isValidName = input => {
+    const regex = /^[a-zA-Z]+(\s[a-zA-Z]+)?$/
+    if (!regex.test(input) && !input) {
+      setHolderError(required)
+    }
+  }
+
+  const onEmailChange = value => {
+    setEmailValue(value)
+    setEmailError('')
+  }
+  const onCardChange = value => {
+    setCardNumber(value)
+    setCardError(false)
+  }
+  const onExpirityChange = value => {
+    setExpiry(value)
+    setExpiryError(false)
+  }
+  const onCVVChange = value => {
+    setCvv(value)
+    setCvvError(false)
+  }
+  const onHolderChange = value => {
+    setHolderName(value)
+    setHolderError(false)
+  }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <div className="inputsBlock">
         <div className="inputsBlock-title">Email</div>
         <InputField
@@ -23,7 +145,9 @@ const Form = () => {
           icon={EmailIcon}
           placeholder="examplemail@mail.com"
           value={emailValue}
-          onHandleChange={setEmailValue}
+          error={emailError}
+          onHandleChange={value => onEmailChange(value)}
+          handleBlur={value => isValidEmail(value)}
         />
       </div>
       <div className="inputsBlock">
@@ -34,7 +158,9 @@ const Form = () => {
           mask="card"
           placeholder="0000 0000 0000 0000"
           value={cardNumber}
-          onHandleChange={setCardNumber}
+          error={cardError}
+          onHandleChange={value => onCardChange(value)}
+          handleBlur={value => isValidCardNumber(value)}
         />
         <div className="inputGroupWrapper">
           <InputField
@@ -42,8 +168,10 @@ const Form = () => {
             icon={ExpiryIcon}
             mask="expiry"
             placeholder="MM/YYYY"
+            error={expiryError}
+            onHandleChange={value => onExpirityChange(value)}
+            handleBlur={value => isValidExpiryDate(value)}
             value={expiry}
-            onHandleChange={setExpiry}
           />
           <InputField
             type="cvv"
@@ -51,7 +179,9 @@ const Form = () => {
             icon={CVVIcon}
             placeholder="CVV"
             value={cvv}
-            onHandleChange={setCvv}
+            error={cvvError}
+            onHandleChange={value => onCVVChange(value)}
+            handleBlur={value => isValidCVV(value)}
           />
         </div>
       </div>
@@ -62,7 +192,9 @@ const Form = () => {
           icon={HolderIcon}
           placeholder="Enter cardholder name"
           value={holderName}
-          onHandleChange={setHolderName}
+          error={holderError}
+          onHandleChange={value => onHolderChange(value)}
+          handleBlur={value => isValidName(value)}
         />
       </div>
       <div className="link">I have a coupon code</div>
--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import classNames from 'classnames'
 import InputMask from 'react-input-mask'
 import ViseIcon from '../assets/img/icons/coloredIcons/bx_bxl-visa.svg'
@@ -9,102 +9,74 @@ import AECIcon from '../assets/img/icons/coloredIcons/american-express-card.svg'
 const InputField = ({
   type,
   value,
+  error,
   placeholder,
   icon,
   mask = false,
   onHandleChange,
+  handleBlur,
 }) => {
-  const [error, setError] = useState(false)
   const handleChange = e => {
     onHandleChange(e.target.value)
   }
 
-  const handleFocutOut = () => {
-    type === 'email' && setError(!isValidEmail(value))
-    type === 'curdNumber' && setError(isValidCardNumber(value))
-    type === 'expiryDate' && setError(!isValidExpiryDate(value))
-    type === 'cvv' && setError(!isValidCVV(value))
-    type === 'name' && setError(!isValidName(value))
-  }
-
-  const isValidEmail = email => {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    return emailRegex.test(email)
-  }
-
-  const isValidCardNumber = value => {
-    const cleanedValue = value.replace(/\D/g, '')
-    return /^\d{16}$/.test(cleanedValue) ? false : true
-  }
-
-  const isValidExpiryDate = input => {
-    const regex = /^(0[1-9]|1[0-2])\/(20\d{2})$/
-    if (!regex.test(input)) {
-      return false
-    }
-
-    const [, month, year] = input.match(regex)
-    const currentYear = new Date().getFullYear()
-    const validYear = currentYear + 5
-
-    return (
-      parseInt(year, 10) >= currentYear &&
-      parseInt(year, 10) <= validYear &&
-      parseInt(month, 10) >= 1 &&
-      parseInt(month, 10) <= 12
-    )
-  }
-
-  const isValidCVV = input => {
-    const regex = /^\d{3}$/
-    return regex.test(input)
-  }
-
-  const isValidName = input => {
-    const regex = /^[a-zA-Z]+(\s[a-zA-Z]+)?$/
-    return regex.test(input)
+  const inputMaskProps = {
+    value,
+    placeholder,
+    mask:
+      mask === 'card'
+        ? '9999 9999 9999 9999'
+        : mask === 'expiry'
+          ? '99/9999'
+          : mask === 'cvv'
+            ? '999'
+            : '',
+    alwaysShowMask: false,
+    onChange: handleChange,
+    onBlur: e => handleBlur(e.target.value),
   }
 
   return (
-    <div className={classNames('input-container', { error: error })}>
-      <InputMask
-        value={value}
-        placeholder={placeholder}
-        mask={
-          mask === 'card'
-            ? '9999 9999 9999 9999'
-            : mask === 'expiry'
-              ? '99/9999'
-              : mask === 'cvv'
-                ? '999'
-                : ''
-        }
-        alwaysShowMask={false}
-        onChange={handleChange}
-        onBlur={handleFocutOut}
-      />
-      <img className="icon" src={icon} alt={type} />
-      {type === 'curdNumber' && (
-        <div className="icon-additional">
-          <img
-            src={ViseIcon}
-            alt="visa icon"
-            className="icon-additional__item"
-          />
-          <img
-            src={DiscoverIcon}
-            alt="Discover icon"
-            className="icon-additional__item"
-          />
-          <img
-            src={MastercardIcon}
-            alt="Mastercard icon"
-            className="icon-additional__item"
-          />
-          <img src={AECIcon} alt="AEC icon" className="icon-additional__item" />
-        </div>
-      )}
-    </div>
+    <>
+      <div className={classNames('input-container', { error: Boolean(error) })}>
+        {Boolean(error) && (
+          <div
+            className={classNames('error-text', {
+              expiration: type === 'expiryDate',
+              cvv: type === 'cvv',
+            })}
+          >
+            {error}
+          </div>
+        )}
+        <InputMask {...inputMaskProps} />
+        <img className="icon" src={icon} alt={type} />
+        {type === 'curdNumber' && (
+          <div className="icon-additional">
+            <img
+              src={ViseIcon}
+              alt="visa icon"
+              className="icon-additional__item"
+            />
+            <img
+              src={DiscoverIcon}
+              alt="Discover icon"
+              className="icon-additional__item"
+            />
+            <img
+              src={MastercardIcon}
+              alt="Mastercard icon"
+              className="icon-additional__item"
+            />
+            <img
+              src={AECIcon}
+              alt="AEC icon"
+              className="icon-additional__item"
+            />
+          </div>
+        )}
+      </div>
+    </>
   )
 }
 
